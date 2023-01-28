48eee221f5aa9722d4f13ae2cf2e6d44aaeb552b
kobakou <kou.kobayashi@sony.com>
2023-01-28 12:09:11 +0900

    first-working-commit

---
+++ runspeedtest.ps1
@@ -0,0 +1,44 @@
+##### include config #####
+. ".\config.ps1"
+
+##### 定数セット #####
+$URI = 'https://gw.machinist.iij.jp/endpoint'
+$HEADER = @{
+    Authorization = "Bearer $API_KEY"
+}
+
+##### データ生成 #####
+$RESULT = (.\speedtest.exe -s 48463 -f csv --output-header | ConvertFrom-Csv | Select-Object @{
+        name='Download'
+        expr={[Int]$_.download * 8}
+    },
+    @{
+        name='RTT'
+        expr={$_.'idle latency'}
+    })
+$DATA = @{
+    agent_id = $AGENT_ID
+    metrics = @(
+        @{
+            name = "Download"
+            namespace = "Speedtest.net"
+            data_point = @{
+                value = $RESULT.Download
+            }
+        },
+        @{
+            name = "RTT"
+            namespace = "Speedtest.net"
+            data_point = @{
+                value = [double]$RESULT.RTT
+            }
+        }
+    )
+}
+$BODY = $DATA | ConvertTo-Json -Depth 5  # Depthオプションが無いとdata_point内がJSON化されない
+
+echo $URI
+echo $BODY
+
+##### データ送信 #####
+Invoke-RestMethod -Method Post -ContentType 'application/json' -Headers $HEADER -Uri $URI -Body $BODY
